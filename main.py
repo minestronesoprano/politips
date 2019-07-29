e2a3c8cea23b767cfcc987a20fe5349beca9b686
cssi <cssi@cssis-MacBook-Pro.local>
2019-07-29 11:40:48 -0400

    main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+# main.py
+# the import section
+import webapp2
+import jinja2
+import os
+
+# This initializes the jinja2 Environment.
+# This will be the same in every app that uses the jinja2 templating library.
+the_jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+# the handler section
+
+class EnterInfoHandler(webapp2.RequestHandler):
+    def get(self):  # for a get request
+        welcome_template = the_jinja_env.get_template('templates/welcome.html')
+        self.response.write(welcome_template.render())  # the response
+
+class MainPage(webapp2.RequestHandler):
+    def get(self): #for a get request
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Hello, World!') #the response
+
+# the app configuration section
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
